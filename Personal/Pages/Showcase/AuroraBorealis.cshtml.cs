0a621948337934365029df4d87ac1c8a092d0db9
MattPalmer03 <mattpalmer950@gmail.com>
2026-02-23 11:35:43 +0200

    pushed new ui

---
+++ Personal/Pages/Showcase/AuroraBorealis.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Personal.Pages.Showcase
+{
+    public class AuroraBorealisModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Personal/Pages/Showcase/NebulaPulse.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Personal.Pages.Showcase
+{
+    public class NebulaPulseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
